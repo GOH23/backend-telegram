47229a15d324aefa2fe53cb4022651a3cd5a7f7e
GOH23 <padadamlpd@gmail.com>
2024-09-01 21:54:29 +0300

    updated api cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const options = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  "credentials":true
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api/v1")
-  app.enableCors({
-    origin: [
-      "https://telegram-sell-bot.vercel.app/"
-    ],
-  });
+  app.enableCors(options);
   await app.listen(3001);
 }
 bootstrap();
